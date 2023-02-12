9ca3c65ae6f1bd00e6d035a1cf3c993e2c92189b
Jakub Flanderka <flandak@gmail.com>
2023-02-12 01:23:24 +0100

    added props and hardcoded array of objects

--- expense_tracker__app/src/App.js
+++ expense_tracker__app/src/App.js
@@ -1,9 +1,29 @@
-import RouteItem from './components/RouteItem'
+import RouteItem from "./components/RouteItem";
 
 function App() {
+  const routes = [
+    {
+      date: new Date(2020, 2, 11),
+      title: "silence",
+      grade: "9c",
+    },
+    {
+      date: new Date(2023, 2, 11),
+      title: "perfecto mundo",
+      grade: "9b+",
+    },
+    {
+      date: new Date(2021, 2, 11),
+      title: "off the wagon",
+      grade: "8c+",
+    },
+  ];
   return (
     <div>
-     <RouteItem></RouteItem>
+      <RouteItem date={routes[1].date} title={routes[1].title} grade={routes[1].grade}></RouteItem>
+      <RouteItem date={routes[0].date} title={routes[0].title} grade={routes[0].grade}></RouteItem>
+      <RouteItem date={routes[2].date} title={routes[2].title} grade={routes[2].grade}></RouteItem>
+      
     </div>
   );
 }
--- expense_tracker__app/src/components/RouteItem.js
+++ expense_tracker__app/src/components/RouteItem.js
@@ -1,11 +1,19 @@
 import "./RouteItem.css";
-function RouteItem() {
+function RouteItem(props) {
+    const month = props.date.toLocaleString('en-US', {month:'long'});
+    const day = props.date.toLocaleString('en-US', {day:'2-digit'});
+    const year = props.date.getFullYear();
   return (
     <div className="route-item">
-      <div>Date</div>
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
+      <div>{props.date.toISOString()}</div>
       <div className="route-item__description">
-        <h2>Title</h2>
-        <div className="route-item__price">Grade</div>
+        <h2>{props.title}</h2>
+        <div className="route-item__grade">{props.grade}</div>
       </div>
     </div>
   );
