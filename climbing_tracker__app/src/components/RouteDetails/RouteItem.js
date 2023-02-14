985b1d795e3238ab33c04a7c4f1e369bcff0ef29
Jakub Flanderka <flandak@gmail.com>
2023-02-14 17:35:21 +0100

    added state

--- climbing_tracker__app/src/components/RouteDetails/RouteItem.js
+++ climbing_tracker__app/src/components/RouteDetails/RouteItem.js
@@ -2,18 +2,24 @@ import "./RouteItem.css";
 import RouteDate from "./RouteDate";
 import RouteStyle from "./RouteStyle";
 import Card from "../UI/Card";
-import React from "react";
-import Header from "../Header";
+import React, { useState } from "react";
+
 function RouteItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("updated");
+    console.log(title);
+  };
   return (
     <Card className="route-item">
-      {/* <Header></Header> */}
       <RouteDate date={props.date} />
       <div className="route-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <RouteStyle style={props.style} />
         <div className="route-item__grade">{props.grade}</div>
       </div>
+      <button onClick={clickHandler}>change title</button>
     </Card>
   );
 }
